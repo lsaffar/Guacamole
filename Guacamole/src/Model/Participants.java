b39ce10dc92aebc7e72af2c31e4f5241525914c2
saffar <leasaffar@hotmail.fr>
2018-01-13 21:17:26 +0100

    Backtracking

--- Guacamole/src/Model/Participants.java
+++ Guacamole/src/Model/Participants.java
@@ -7,28 +7,28 @@ import java.util.ArrayList;
 
 /**
  * @author lea
- * 
- * Liste des participants
  *
  */
 public class Participants<E> {
 
 	private ArrayList<E> participantsList;
-	
-	/**
-	 * Constructeur par default (List vide)
-	 */
+
 	public Participants() {
-		this.participantsList = new ArrayList<E>();
+		this.participantsList = new ArrayList<>();
 	}
 
-	/** 
-	 * @return Liste des participants
+	public Participants(ArrayList<E> participantsList) {
+		this.participantsList = participantsList;
+	}
+
+	/**
+	 * 
+	 * @return
 	 */
 	public ArrayList<E> getParticipantsList() {
 		return participantsList;
 	}
-	
+
 	/**
 	 * 
 	 * @param participantsList
@@ -36,24 +36,33 @@ public class Participants<E> {
 	public void setParticipantsList(ArrayList<E> participantsList) {
 		this.participantsList = participantsList;
 	}
-	
+
 	/**
-	 * Rajoute un element dans la liste
+	 * 
 	 * @param element
 	 */
 	public void addParticipantsList(E element) {
 		this.participantsList.add(element);
 	}
 
+	/**
+	 * 
+	 * @param element
+	 * @return <code>true</code> si <code>element</code> est dans la liste.
+	 */
+	public boolean contains(E element) {
+		return participantsList.contains(element);
+	}
+
 	@Override
 	public String toString() {
 		String toString = "";
 		int tailleList = participantsList.size();
-		
-		for(int i = 0; i < tailleList; i++) {
+
+		for (int i = 0; i < tailleList; i++) {
 			toString += participantsList.get(i) + " | ";
 		}
-		
+
 		return toString;
 	}
 }
